5967a8c5a91406b01fc20e92e6166362d29c741e
luizfelipecs <luizfelipecavs@gmail.com>
2021-06-15 13:35:19 +0000

    t1 - Beta 1.3

--- Main.hs
+++ Main.hs
@@ -1,146 +1,55 @@
 import SvgFunctions
 import Palettes
+import GenForms
 import Text.Printf
 
 {-
 FALTA:
-  - Desenhar navio (retângulos e círculos)
   - Receber cor do navio por parâmetro
   - Resolver localização do sol e da lua (não sair dos limites, voltar para o inicio)
-  - Resolver getLine (passagem de parâmetros do usuário)
   - Limpar o código
+
+  verificar valores fora do limite
 -}
 
---  Muita coisa por corrigir aqui
--------------------------------------------------------------------------------
--- Geração do ceu
--------------------------------------------------------------------------------
-genSky :: Int -> [Rect]
-genSky n  = [((m*(w+gap), 0.0), w, h) | m <- [0..fromIntegral (n-1)]]
-  where (w,h) = (1000,500)
-        gap = 10
-
--------------------------------------------------------------------------------
--- Geração do sol
--------------------------------------------------------------------------------
-genSun :: Float -> Int -> [Circle]
-genSun hora n  = [((w-gap * abs(hora-5),100 + abs(12-hora)*20), r) | m <- [0..fromIntegral (n-1)]]
-  where r = 60
-        w = 1000
-        gap = 70
-        space = (gap - r)/2
-
-
--------------------------------------------------------------------------------
--- Geração do mar
--------------------------------------------------------------------------------
-genSea :: Int -> [Rect]
-genSea n  = [((0.0, 500.0), w, h) | m <- [0..fromIntegral (n-1)]]
-  where (w,h) = (1000,500)
-        gap = 10
-
-
--------------------------------------------------------------------------------
--- Geração de retângulos
--------------------------------------------------------------------------------
-genRectsInLine :: Float -> Float -> Float -> Int -> [Rect]
-genRectsInLine base x y n  = [((m*(gap+tam)+x, y-tam), tam, tam) | m <- [0..fromIntegral (n-1)]]
-  where gap = 10
-        tam = base/10
-
--------------------------------------------------------------------------------
--- Geracao de trapézios
--------------------------------------------------------------------------------
---genTrapeze n = [((0,0), (50,50), (150,50), (200,0)) | m <- [0..fromIntegral (n-1)]]
-
-genTrapeze :: Float -> Float -> Float -> Int -> [Trapeze]
-genTrapeze base x y n = [((x,y), (x + base/4,y+h), (x+(base*3/4),y+h), (x + base,y)) | m <- [0..fromIntegral (n-1)]]
-  where h = base/4
-
-
-defineSkyColor :: Int -> [(Int,Int,Int)]
-defineSkyColor hora 
-  | hora >= 7 && hora < 12   = head rgbPaletteSky2 : [] -- manha
-  | hora == 12               = head (tail rgbPaletteSky2) : []
-  | hora > 12 && hora <= 17  = head (tail (tail rgbPaletteSky2)) :[]
-  | otherwise                = last rgbPaletteSky2 : []
-
-
-defineSunColor :: Int -> [(Int,Int,Int)]
-defineSunColor hora 
-  | hora >= 7 && hora < 12   = head rgbPaletteSun2 : []
-  | hora == 12               = head (tail rgbPaletteSun2) :[]
-  | hora > 12 && hora <= 17  = last rgbPaletteSun2 : []
-  | otherwise                = rgbPaletteMoon
-  
-
-stringToInt :: String -> Int
-stringToInt str = read str 
-
-stringToFloat :: String -> Float
-stringToFloat str = read str 
-
---stringToRGB :: String -> (Int,Int,Int)
---stringToRGB str = read str
 -------------------------------------------------------------------------------
 -- Função principal que gera arquivo com imagem SVG --  CORRIGIR
 -------------------------------------------------------------------------------
 main :: IO ()
 main = do
-  
-  putStrLn "Digite o horario do dia em que o navio esta navegando: "
-  hora' <- getLine -- o que eu faço aqui?
-  hora  =  read hora'
-  putStrLn "Digite o tamanho da base do navio (base menor de um trapezio): "
-  base' <- getLine
-  let base = stringToFloat base'
-  putStrLn "Digite a posicao do navio (x,y) (Aposicao se refere ao ponto mais a esquerda da base maior do trapezio): "
-  
-  putStrLn "Digite a cor do navio no formato rgb"
-  
-  
-  {-
-  defineSkyColor turno, otherwise manha
-  
-  chama desenho e cor do ceu e o sol
-  -}
-
-  
-  --base <- getLine
-  
-  --(x,y) <- getLine
-  
-
-
-  {-
-  verifica tamanho da base e a posicao
-  chama desenho
-  -}
-
   writeFile "t1luizfelipe.svg" $ svgstrs
-  where svgstrs   = svgBegin w h ++ svgsky ++ svgsun ++ svgsea ++ svgfigs ++ svgrect ++ svgEnd
-        
-        svgfigs   = svgElements svgTrapeze trapeze (map svgStyleTrapeze palette) 
-        trapeze   = genTrapeze base 100 700 ntrapeze
-        palette   = rgbPalette ntrapeze
+  where svgstrs  = svgBegin w h ++ svgsky ++ svgsun ++ svgsea ++ svgship ++ svgrect ++ svgEnd
+        clock = 16
+        --if clock < 0 || clock > 23 then clock = 12 else clock
+        --falta o readme a equação do sol
+        base = 1000
+        posX = 200
+        --if posX < 0 || posX > w(1000) then posX = 0(canto esquerdo)
+        posY = 800
+        --if posY < 500 || posY > h(1000) then posY = 750(metade do mar) 
+        shipColor = (255,345,32) : []
+        numContainer = 8
+
+        svgship   = svgElements svgTrapeze trapeze (map svgStyleTrapeze palette) 
+        trapeze   = genTrapeze base posX posY ntrapeze
+        palette   = shipColor
         ntrapeze  = 1
 
         svgsky    = svgElements svgRect sky (map svgStyleRect palette2)
-        sky       = genSky 1
-        palette2  = defineSkyColor 20
-        
+        sky       = genSky
+        palette2  = defineSkyColor clock
 
         svgsun    = svgElements svgCircle sun (map svgStyleCircle palette3)
-        sun       = genSun hora 1
-        palette3  = defineSunColor hora
-
+        sun       = genSun clock 1
+        palette3  = defineSunColor clock
+        
         svgsea    = svgElements svgRect sea (map svgStyleRect palette4)
-        sea       = genSea 1
+        sea       = genSea
         palette4  = rgbPaletteSea
         
-        svgrect    = svgElements svgRect navio (map svgStyleRect palette5)
-        navio       = genRectsInLine base 100 700 4
-        palette5  = rgbPaletteRect 4
+        svgrect   = svgElements svgRect navio (map svgStyleRect palette5)
+        navio     = genRectsInLine base posX posY numContainer
+        palette5  = rgbPaletteRect numContainer
 
-        (w,h) = (1000,1000)
+        (w,h)     = (1000,1000)
       
--- palettes.hs
+++ palettes.hs
@@ -8,14 +8,14 @@ rgbPaletteRect n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255),(255,255,0)]
 --rgbPaletteSky :: Int -> [(Int,Int,Int)]
 --rgbPaletteSky n = take n $ cycle [(135,206,235),(0,191,255),(65,105,225),(25,25,112)]
 
-rgbPaletteSky2 :: [(Int,Int,Int)]
-rgbPaletteSky2 =  [(135,206,235),(0,191,255),(65,105,225),(28,28,28)]
+rgbPaletteSky :: [(Int,Int,Int)]
+rgbPaletteSky =  [(135,206,235),(0,191,255),(65,105,225),(28,28,28)]
 
 --rgbPaletteSun :: Int -> [(Int,Int,Int)]
 --rgbPaletteSun n = take n $ cycle [(255,215,0),(255,140,0),(255,69,0)]
 
-rgbPaletteSun2 :: [(Int,Int,Int)]
-rgbPaletteSun2 =  [(255,215,0),(255,140,0),(255,69,0)]
+rgbPaletteSun :: [(Int,Int,Int)]
+rgbPaletteSun =  [(255,215,0),(255,140,0),(255,69,0)]
 
 rgbPaletteMoon :: [(Int,Int,Int)]
 rgbPaletteMoon =  [(220,220,220)]
@@ -23,3 +23,18 @@ rgbPaletteMoon =  [(220,220,220)]
 rgbPaletteSea :: [(Int,Int,Int)]
 rgbPaletteSea =  [(25,25,112)]
 --rgbPaletteSea = [(25,25,112)]
+
+defineSkyColor :: Int -> [(Int,Int,Int)]
+defineSkyColor hora 
+  | hora >= 7 && hora < 12   = head rgbPaletteSky : []
+  | hora >= 12 && hora <= 15 = head (tail rgbPaletteSky) : []
+  | hora > 15 && hora <= 17  = head (tail (tail rgbPaletteSky)) :[]
+  | otherwise                = last rgbPaletteSky : []
+
+
+defineSunColor :: Int -> [(Int,Int,Int)]
+defineSunColor hora 
+  | hora >= 7 && hora < 12   = head rgbPaletteSun : []
+  | hora >= 12 && hora <= 15 = head (tail rgbPaletteSun) :[]
+  | hora > 15 && hora <= 17  = last rgbPaletteSun : []
+  | otherwise                = rgbPaletteMoon
